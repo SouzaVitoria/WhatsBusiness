f6371267baf61d305ce04a83963bd3f14b414f4f
Vitoria Souza <viihsouza161@gmail.com>
2019-05-07 17:51:07 -0300

    Introduction Redux
    React library communication with the Redux Framework

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import Rotas from './src/Routes';
+
 
 export default class App extends Component{
   render() {
     return (
+      <Provider store={createStore(reducers)}>
         <Rotas/>
+      </Provider>
     );
   }
 }
