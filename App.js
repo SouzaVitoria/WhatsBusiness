e9f54f25df2f03da594c9f5b9553bd84079abfa6
Vitoria Souza <viihsouza161@gmail.com>
2019-05-09 14:55:14 -0300

    Conectando o App ao Firebase
    Conectando o App ao Firebase.

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import React, {Component} from 'react';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import Rotas from './src/Routes';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
 
+import Routes from './src/Routes';
+import reducers from './src/reducers';
 
-export default class App extends Component{
+export default class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    firebase.initializeApp({
+      apiKey: "AIzaSyAtXWDPUNCsQHP-Siq1qC-ssG1RW4uCs-Y",
+      authDomain: "whatsbusiness-20190509.firebaseapp.com",
+      databaseURL: "https://whatsbusiness-20190509.firebaseio.com",
+      projectId: "whatsbusiness-20190509",
+      storageBucket: "whatsbusiness-20190509.appspot.com",
+      messagingSenderId: "519682617290",
+      appId: "1:519682617290:web:f119405a68a64778"
+    });
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <Rotas/>
+        <Routes />
       </Provider>
     );
   }
