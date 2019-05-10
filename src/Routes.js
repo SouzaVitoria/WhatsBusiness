570dd48e568f0d3ebf04203c06ea9eb69477e220
Vitoria Souza <viihsouza161@gmail.com>
2019-05-10 15:32:31 -0300

    add router of Welcome

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,16 @@ import { Router, Scene } from 'react-native-router-flux';
 
 import FormLogin from './components/FormLogin';
 import FormCadastro from './components/FormCadastro';
+import BoasVindas from './components/BoasVindas';
 
-const Rotas = () => (
+const Routes = () => (
   <Router scene>
     <Scene>
-      <Scene key='formCadastro' component={FormCadastro} title='Cadastro'></Scene>
-      <Scene key='formLogin' component={FormLogin} title='Login' initial></Scene>
+      <Scene key='formCadastro' component={FormCadastro} title='Cadastro' />
+      <Scene key='formLogin' component={FormLogin} title='Login' initial />
+      <Scene key='formBoasVindas' component={BoasVindas} title='Welcome' />
     </Scene>
   </Router>
 );
 
-export default Rotas;
+export default Routes;
