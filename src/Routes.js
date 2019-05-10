692caec6e3402becb46e3284393a1ec6c553dafc
Vitoria Souza <viihsouza161@gmail.com>
2019-05-10 16:49:28 -0300

    Tela Principal
    Tela principal e adicionando mais uma rota

--- src/Routes.js
+++ src/Routes.js
@@ -4,13 +4,18 @@ import { Router, Scene } from 'react-native-router-flux';
 import FormLogin from './components/FormLogin';
 import FormCadastro from './components/FormCadastro';
 import BoasVindas from './components/BoasVindas';
+import Principal from './components/Principal';
 
 const Routes = () => (
-  <Router scene>
+  <Router
+    navigatorBarStyle={{ backgroundColor: '#115E54' }}
+    titleStyle={{ color: '#FFF' }}
+  >
     <Scene>
       <Scene key='formCadastro' component={FormCadastro} title='Cadastro' />
       <Scene key='formLogin' component={FormLogin} title='Login' initial />
       <Scene key='formBoasVindas' component={BoasVindas} title='Welcome' />
+      <Scene key='formPrincipal' component={Principal} title='Main' />
     </Scene>
   </Router>
 );
---
+++ src/components/Principal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Principal extends Component {
+ render(){
+  return(
+   <View>
+    <Text> Here main </Text>
+   </View>
+  )
+ }
+}
+
+export default Principal;
