f723fde91d05ddb2eacf7e6375b7bbf728e06046
Vitoria Souza <viihsouza161@gmail.com>
2019-05-10 15:28:26 -0300

    base64, message error
     - Save name (base64 for email(id))

--- src/actions/AutenticacaoActions.js
+++ src/actions/AutenticacaoActions.js
@@ -1,41 +1,64 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
+import b64 from 'base-64';
 
 export const modificaEmail = textEmail => {
- return {
-  type: 'modifica_email',
-  payload: textEmail
- }
+  return {
+    type: 'modifica_email',
+    payload: textEmail
+  }
 }
 
 export const modificaPassword = textPassword => {
- return {
-  type: 'modifica_password',
-  payload: textPassword
- }
+  return {
+    type: 'modifica_password',
+    payload: textPassword
+  }
 }
 
 export const modificaName = textName => {
- return {
-  type: 'modifica_name',
-  payload: textName
- }
+  return {
+    type: 'modifica_name',
+    payload: textName
+  }
 }
 
 export const cadastraUser = ({ name, email, password }) => {
- return dispatch => {
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-   .then(user => cadastroUserSuccess(dispatch))
-   .catch(erro => cadastroUserError(erro, dispatch));
- }
-
+  return dispatch => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        let emailB64 = b64.encode(email);
+        firebase.database().ref(`/contatos/${emailB64}`)
+          .push({ name })
+          .then(value => cadastroUserSuccess(dispatch));
+      })
+      .catch(erro => cadastroUserError(erro, dispatch));
+  }
 }
 
 const cadastroUserSuccess = dispatch => {
- dispatch({ type: 'cadastro_user_success' });
+  dispatch({ type: 'cadastro_user_success' });
   Actions.formBoasVindas();
 }
 
 const cadastroUserError = (erro, dispatch) => {
- dispatch({ type: 'cadastro_user_error', payload: erro.message });
+  dispatch({ type: 'cadastro_user_error', payload: erro.message });
+}
+
+export const autenticarUser = ({ email, password }) => {
+  return dispatch => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(value => {
+        dispatch({
+          type: 'login_user_success'
+        })
+      })
+      .catch(erro => {
+        erro,
+          dispatch({
+            type: 'login_user_erro',
+            payload: erro.message
+          })
+      });
+  }
 }
